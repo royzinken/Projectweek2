6b8b501dfd659d32fe0c2d53dd3a064a5885eeef
mkcnelis <maikel.nelis@gmail.com>
2020-01-28 11:27:46 +0100

    tried to fix puzzle
    the sriptTextQueastions .js does ntot fully want to run have to check/ ask what is blocking its run

--- Javascript/scriptTextQuestions.js
+++ Javascript/scriptTextQuestions.js
@@ -1,20 +1,3 @@
-document.addEventListener('DOMContentLoaded', 
-    function(){
-      Typed.new('.entry', {
-        strings: [ "------......----"+"<br>"+"...----..--",],
-        typeSpeed: 1
-      });
-  });
-  
-
-
-  document.addEventListener('DOMContentLoaded', //the second  ling of entries of scroll text
-  function(){
-    Typed.new('.entry2', {
-      strings: [ " ​test voor de scramble tekst",],
-      typeSpeed: 1
-    });
-});
 
   // DEFAULT SETTINGS
   $('#wrong').hide();
