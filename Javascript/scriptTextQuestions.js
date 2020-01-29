f9e7dbd72198dd826f4dc97f6d6d168f63e3e1a0
mkcnelis <maikel.nelis@gmail.com>
2020-01-29 16:13:45 +0100

    fix up with help
    Co-Authored-By: royzinken <royzinken@users.noreply.github.com>
    Co-Authored-By: damien1304 <damien1304@users.noreply.github.com>

--- Javascript/scriptTextQuestions.js
+++ Javascript/scriptTextQuestions.js
@@ -1,32 +1,24 @@
-// this java script is for scramblecodepuzzle.html
+// this javascript if for morsecodePuzzle.html
   // DEFAULT SETTINGS
   $('#wrong').hide();
   $('#correct').hide();
 
-  // SELECT INPUT BOX + CLEAR AFTER WRONG ANSWER
-  $('text.superhero').focus(function () {
-      $('#wrong').hide();
-      $('#correct').hide();
-      $("button.submit").show();
-  });
-
 function guessAnswer () {
   
-  // CHECK ANSWER
-  $("button.submit").click(function () {
-    var answer = document.getElementById("cookie").value;
+  var answer = document.getElementById("cookie").value;
     
   //IF CORRECT ANSWER
 		if (answer == 'Why are you running') {
-			$('#correct').show();
+      $('#correct').show();
+      $('#wrong').hide();
+     // window.location.href = "http://www.w3schools.com";
       
   //IF WRONG ANSWER
       } else {
-          $('#wrong').show();}
+          $('#wrong').show();
+          $('#correct').hide();
+          document.getElementById("cookie").value = "";
+        }
           
 
-  });
-  
 }
-
-guessAnswer();
--- Javascript/scriptTextQuestions2.js
+++ Javascript/scriptTextQuestions2.js
@@ -3,30 +3,22 @@
   $('#wrong').hide();
   $('#correct').hide();
 
-  // SELECT INPUT BOX + CLEAR AFTER WRONG ANSWER
-  $('text.superhero').focus(function () {
-      $('#wrong').hide();
-      $('#correct').hide();
-      $("button.submit").show();
-  });
-
 function guessAnswer () {
   
-  // CHECK ANSWER
-  $("button.submit").click(function () {
-    var answer = document.getElementById("cookie").value;
+  var answer = document.getElementById("cookie").value;
     
   //IF CORRECT ANSWER
 		if (answer == 'dot') {
-			$('#correct').show();
+      $('#correct').show();
+      $('#wrong').hide();
+     // window.location.href = "http://www.w3schools.com";
       
   //IF WRONG ANSWER
       } else {
-          $('#wrong').show();}
+          $('#wrong').show();
+          $('#correct').hide();
+          document.getElementById("cookie").value = "";
+        }
           
 
-  });
-  
 }
-
-guessAnswer();
