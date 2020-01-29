126af4054cde73bbdbfeda9c50d0af06bacb13c2
BasMolenaar <56149215+BasMolenaar@users.noreply.github.com>
2020-01-29 12:18:56 +0100

    add Levels.js

---
+++ java/Levels.js
@@ -0,0 +1,21 @@
+function UnlockLevelTwo() {
+    parent.test.location=file;
+}
+
+function UnlockLevelThree() {
+    parent.test.location=file;
+}
+
+function UnlockLevelFour() {
+    parent.test.location=file;
+}
+
+function UnlockLevelFive() {
+    parent.test.location=file;
+}
+
+function UnlockLevelSix() {
+    parent.test.location=file;
+}
+
+
