78a3232d3e5ba087cecd2a28feb975633e9f036a
mkcnelis <maikel.nelis@gmail.com>
2020-01-30 16:16:33 +0100

    deleted go back now

--- Javascript/scriptTextQuestions.js
+++ Javascript/scriptTextQuestions.js
@@ -1,24 +1,53 @@
-// this javascript if for morsecodePuzzle.html
+document.addEventListener('DOMContentLoaded', 
+    function typedtext(){
+      Typed.new('.entry', {
+        strings: [ "------......----"+"<br>"+"...----..--",],
+        typeSpeed: 1
+      });
+  });
+  
+
+
+  document.addEventListener('DOMContentLoaded', //the second  ling of entries of scroll text
+  function typedtext2(){
+    Typed.new('.entry2', {
+      strings: [ " ​test voor de scramble tekst",],
+      typeSpeed: 1
+    });
+});
+
   // DEFAULT SETTINGS
   $('#wrong').hide();
   $('#correct').hide();
 
-function guessAnswer () {
+  // SELECT INPUT BOX + CLEAR AFTER WRONG ANSWER
+  $('text.superhero').focus(function () {
+      $('#wrong').hide();
+      $('#correct').hide();
+      $("button.submit").show();
+  });
+
+function scrambleguessAnswer () {
   
-  var answer = document.getElementById("cookie").value;
+  // CHECK ANSWER
+  $("button.submit").click(function () {
+    var answer = document.getElementById("cookie").value;
     
   //IF CORRECT ANSWER
-		if (answer == 'Why are you running') {
-      $('#correct').show();
-      $('#wrong').hide();
-     window.location.href = "../succes.html";
+		if (answer == 'dat') {
+			$('#correct').show();
       
   //IF WRONG ANSWER
       } else {
-          $('#wrong').show();
-          $('#correct').hide();
-          document.getElementById("cookie").value = "";
-        }
+          $('#wrong').show();}
           
 
+  });
+  
+}
+
+scrambleguessAnswer();
+
+
+function Morsecheck(){
 }
