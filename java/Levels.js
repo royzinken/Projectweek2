01d472eb83304e26966552f063bd81394e24f509
bubastes <56149277+bubastes@users.noreply.github.com>
2020-01-30 21:05:11 +0100

    pogingen tot funcioneel maken van <area> verbinding. niet gelukt

--- java/Levels.js
+++ java/Levels.js
@@ -16,3 +16,7 @@ function UnlockLevelFive(file) {
 
 document.getElementById("6_wires_frame_1_next").onclick = function() {UnlockLevelTwo('6Wired_v3_jari_runa.html')};
 
+$(".correct").on("click", function(e){
+    e.preventDefault();
+    UnlockLevelTwo('./6WiredModule.html');
+});
